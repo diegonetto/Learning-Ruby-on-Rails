dc1e32f0ddb0bf97b3e2038cae21a4f919a372c8
Diego Netto <diegormnetto@gmail.com>
2012-03-17 02:18:48 -0400

    Added length validation to Title field with custom error message.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,13 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
+  validates :title, length: { 
+    minimum: 10, 
+    too_short: 'must be at least 10 characters long.'
+  }
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
-  with: %r{\.(gif|jpg|png)$}i,
-  message: 'must be a URL for GIF, JPG or PNG image.'
-}
+    with: %r{\.(gif|jpg|png)$}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
