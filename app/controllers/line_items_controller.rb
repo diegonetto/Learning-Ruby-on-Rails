7a6384461bef51e05122c3bd9f0b0cf1fb9d9d9c
Diego Netto <diegormnetto@gmail.com>
2012-03-17 17:16:39 -0400

    Completed the excercises at the end of Chapter 9. Added a view counter to catalog page for each session, reset it during line_item creation, and displayed it in the view only if the counter was greater than 5.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -46,6 +46,9 @@ class LineItemsController < ApplicationController
     product = Product.find(params[:product_id])
     @line_item = @cart.line_items.build(product: product)
 
+    # Reset catalog view counter in session
+    session[:counter] = 0  
+
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
   def index
+    if session[:counter].nil?
+      session[:counter] = 1
+    end
+    session[:counter] += 1
+    @counter = session[:counter]
     @products = Product.order(:title)
   end
 end
